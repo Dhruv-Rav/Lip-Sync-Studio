5474b144a4bcad1610262614213a57d00c8e2ef8
Dhruv Raval <111689240+Dhruv-Rav@users.noreply.github.com>
2025-04-30 12:19:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    GOOEY_API_KEY = 'sk-C1WwUld5GRchdHz7nVM2oBx0dGwaRKH0j0MOxxA6od56YnIr'
+    GOOEY_API_KEY = sk-C1WwUld5GRchdHz7nVM2oBx0dGwaRKH0j0MOxxA6od56YnIr
     DEBUG = True  # Enable debug mode
